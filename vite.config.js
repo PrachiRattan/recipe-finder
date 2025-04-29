e2ce3fdc3ab0df3d836d04648c537b8bb5b98d91
Prachi Rattan <prachirattan07@gmail.com>
2025-04-29 23:39:12 +0530

    changes in vite file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 10000, 
-    host: '0.0.0.0',  
+  preview: {
+    host: '0.0.0.0', // required for external access
+    port: 4173,       // optional, keep default
+    allowedHosts: ['recipe-finder-iywv.onrender.com']
   }
 })
